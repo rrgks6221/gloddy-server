a47784abda11ca0579335ff5475f7c20fa304659
lsh <rrgks@naver.com>
2022-03-31 16:27:00 +0900

    eslintrc 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,4 +21,9 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
 };
